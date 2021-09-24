74a2be9c4afb5bd47e47935917c6d462c6f887c0
RodrigoContiRibeiro <rodrigo.conti.ribeiro@gmail.com>
2021-09-24 09:56:08 -0300

    initial commit

---
+++ src/main/java/com/litter/litter/LitterApplication.java
@@ -0,0 +1,13 @@
+package com.litter.litter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/litter/litter/LitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litter.litter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
