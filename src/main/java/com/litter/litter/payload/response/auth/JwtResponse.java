dca30668a0cc36c282f56e1f498c6e4b1cb0ea95
RodrigoContiRibeiro <rodrigo.conti.ribeiro@gmail.com>
2021-11-03 10:05:29 -0300

    Did almost nothing

--- src/main/java/com/litter/litter/payload/response/auth/JwtResponse.java
+++ src/main/java/com/litter/litter/payload/response/auth/JwtResponse.java
@@ -10,19 +10,19 @@ public class JwtResponse {
     private String email;
     private List<String> roles;
 
-    public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
-        this.token = accessToken;
+    public JwtResponse(String token, Long id, String username, String email, List<String> roles) {
+        this.token = token;
         this.id = id;
         this.username = username;
         this.email = email;
         this.roles = roles;
     }
 
-    public String getAccessToken() {
+    public String getToken() {
         return token;
     }
 
-    public void setAccessToken(String AccessToken) {
+    public void setToken(String token) {
         this.token = token;
     }
 
